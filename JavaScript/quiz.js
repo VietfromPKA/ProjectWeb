e77dad485a59a9b77060e74f9fedfb2a70b7e8b1
VietfromPKA <22010256@st.phenikaa-uni.edu.vn>
2024-05-11 14:28:12 +0700

    update js + html

---
+++ JavaScript/quiz.js
@@ -0,0 +1,18 @@
+document.querySelector('.ball').addEventListener('click', function() {
+    window.location.href = './html/ball.html';
+});
+document.querySelector('.math').addEventListener('click', function() {
+    window.location.href = './html/math.html';
+});
+document.querySelector('.music').addEventListener('click', function() {
+    window.location.href = './html/music.html';
+});
+document.querySelector('.game').addEventListener('click', function() {
+    window.location.href = './html/game.html';
+});
+document.querySelector('.food').addEventListener('click', function() {
+    window.location.href = './html/food.html';
+});
+document.querySelector('.digraphs').addEventListener('click', function() {
+    window.location.href = './html/digraphs.html';
+});
