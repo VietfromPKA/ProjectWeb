b5aab30e8f13774f9d1a0e8e73a91f6097f622db
VietfromPKA <22010256@st.phenikaa-uni.edu.vn>
2024-05-23 10:22:04 +0700

    thêm giao diện tính năng nổi bật

--- JavaScript/quiz.js
+++ JavaScript/quiz.js
@@ -15,4 +15,16 @@ document.querySelector('.food').addEventListener('click', function() {
 });
 document.querySelector('.digraphs').addEventListener('click', function() {
     window.location.href = './html/digraphs.html';
+});$(document).ready(function () {
+  $(".image-slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    draggable: false,
+    prevArrow: `<button type='button' class='slick-prev slick-arrow'><</button>`,
+    nextArrow: `<button type='button' class='slick-next slick-arrow'>></button>`,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
 });
