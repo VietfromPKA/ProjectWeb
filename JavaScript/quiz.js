a0df2ff7a200976cf4ec6a65d6bb7b188ed440e0
VietfromPKA <22010256@st.phenikaa-uni.edu.vn>
2024-05-24 07:28:38 +0700

    cập nhật css của feedback

--- JavaScript/quiz.js
+++ JavaScript/quiz.js
@@ -15,7 +15,8 @@ document.querySelector('.food').addEventListener('click', function() {
 });
 document.querySelector('.digraphs').addEventListener('click', function() {
     window.location.href = './html/digraphs.html';
-});$(document).ready(function () {
+});
+$(document).ready(function () {
   $(".image-slider").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -28,3 +29,16 @@ document.querySelector('.digraphs').addEventListener('click', function() {
     autoplaySpeed: 1000,
   });
 });
+$(document).ready(function () {
+    $(".teacher-slide").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><</button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'>></button>`,
+      autoplay: true,
+      autoplaySpeed: 1000,
+    });
+  });
