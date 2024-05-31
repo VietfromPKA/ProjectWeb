82b8bc45385f6a62c8ae7bae2c82191fcb04d7fa
VietfromPKA <22010256@st.phenikaa-uni.edu.vn>
2024-05-31 09:19:31 +0700

    fix lỗi xung đột

--- JavaScript/dropcreat.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var element = document.getElementById("mycreat");
-    document.getElementById("mycreat").classList.toggle("demo");
- }
- function setFunction() {
-    var element = document.getElementById("mysett");
-    document.getElementById("myset").classList.toggle("demo2");
- }
