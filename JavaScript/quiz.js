be94e956f8f3e62c207cbce2c522d617f23bbb72
VietfromPKA <22010256@st.phenikaa-uni.edu.vn>
2024-06-01 09:19:15 +0700

    cập nhật ds_quiz thêm đối tác

--- JavaScript/quiz.js
+++ JavaScript/quiz.js
@@ -52,35 +52,16 @@ $(document).ready(function () {
     var element = document.getElementById("mysett");
     document.getElementById("myset").classList.toggle("demo2");
  }
- document.addEventListener('DOMContentLoaded', function() {
-  // Lấy tất cả các nút trong sidebar
-  const buttons = document.querySelectorAll('.sidebar button');
 
-  // Thêm sự kiện click cho mỗi nút
-  buttons.forEach(button => {
-      button.addEventListener('click', function() {
-          // Lấy giá trị của thuộc tính data-content để xác định phần nội dung cần hiển thị
-          const contentId = this.getAttribute('data-content');
-          
-          // Ẩn tất cả các phần nội dung
-          const contents = document.querySelectorAll('.class-content');
-          contents.forEach(content => {
-              content.style.display = 'none';
-          });
-          
-          // Hiển thị phần nội dung tương ứng
-          const targetContent = document.querySelector(contentId);
-          if (targetContent) {
-              targetContent.style.display = 'block';
-          }
-      });
-  });
-
-  // Hiển thị phần nội dung đầu tiên khi tải trang
-  const defaultContentId = '#lop0'; // ID của nội dung bạn muốn hiển thị mặc định
-  const defaultContent = document.querySelector(defaultContentId);
-  if (defaultContent) {
-      defaultContent.style.display = 'block';
+ function toggleLink() {
+  const link = document.getElementById('myLink');
+  const checkbox = document.getElementById('myToggleSwitch');
+  if (checkbox.checked) {
+      link.href = '../img/nhac.mp3';
+  } else {
+      link.href = '#';
   }
-});
-
+}
+// Thêm sự kiện listener vào checkbox để gọi hàm toggleLink khi trạng thái thay đổi
+document.getElementById('myToggleSwitch').addEventListener('change', toggleLink);
+ 
